26e2b56d900f52cf6667b4730ed284b4f1f5110c
Andrew Huang <k.andrew.huang@gmail.com>
2020-10-30 14:08:44 -0600

    Add VideoRooms API

--- video_rooms.go
+++ video_rooms.go
@@ -1,5 +1,11 @@
 package twilio
 
+import (
+	"context"
+	"net/url"
+	"time"
+)
+
 type VideoRoomService struct {
 	client *Client
 }
@@ -7,7 +13,34 @@ type VideoRoomService struct {
 const videoRoomPathPart = "Rooms"
 
 type VideoRoom struct {
-	Sid        string `json:"sid"`
-	EnableTurn bool   `json:"enable_turn"`
-	Type       string `json:"type"`
+	Sid                         string    `json:"sid"`
+	Status                      string    `json:"status"`
+	DateCreated                 time.Time `json:"date_created"`
+	DateUpdated                 time.Time `json:"date_updated"`
+	AccountSid                  string    `json:"account_sid"`
+	EnableTurn                  bool      `json:"enable_turn"`
+	Type                        string    `json:"type"`
+	UniqueName                  string    `json:"unique_name"`
+	StatusCallback              string    `json:"status_callback"`
+	StatusCallbackMethod        string    `json:"status_callback_method"`
+	EndTime                     time.Time `json:"end_time"`
+	MaxParticipants             int       `json:"max_participants"`
+	RecordParticipantsOnConnect bool      `json:"record_participants_on_connect"`
+	VideoCodecs                 []string  `json:"video_codecs"`
+	MediaRegion                 string    `json:"media_region"`
+	Url                         string    `json:"url"`
+	Links                       []string  `json:"links"`
+}
+
+func (vr *VideoRoomService) Get(ctx context.Context, sid string) (*VideoRoom, error) {
+	room := new(VideoRoom)
+	err := vr.client.GetResource(ctx, videoRoomPathPart, sid, room)
+	return room, err
+}
+
+func (vr *VideoRoomService) Create(ctx context.Context, data url.Values) (*VideoRoom, error) {
+	room := new(VideoRoom)
+	err := vr.client.CreateResource(ctx, videoRoomPathPart, data, room)
+	return room, err
+
 }
